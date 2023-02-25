a472057d8d239d6c6922fdc234e03ec4c6c391fd
Willian Fonseca <willian.alves2109@gmail.com>
2023-02-25 14:13:14 -0300

    Finalizando curso DJANGO FUNDAMENTOS

--- tw-django-fundamentos/parte_02/clientes/app/urls.py
+++ tw-django-fundamentos/parte_02/clientes/app/urls.py
@@ -1,9 +1,12 @@
-from django.urls import include, path
+from django.urls import path
 
-from .views import ClienteCreateView, ClienteListView
+from .views import (ClienteCreateView, ClienteDeletelView, ClienteDetailView,
+                    ClienteListView, ClienteUpdatView)
 
 urlpatterns = [
     path("form_cliente", ClienteCreateView.as_view()),
     path("lista_clientes", ClienteListView.as_view(), name="lista_clientes"),
-    
+    path("form_clientes/<int:pk>", ClienteUpdatView.as_view(), name="editar_cliente"),
+    path("lista_cliente/<int:pk>", ClienteDetailView.as_view(), name="listar_clientes"),
+    path("delete_cliente/<int:pk>", ClienteDeletelView.as_view(), name="delete_cliente"),
 ]
--- tw-django-fundamentos/parte_02/clientes/app/views.py
+++ tw-django-fundamentos/parte_02/clientes/app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
+                                  UpdateView)
 
 from .models import Cliente
 
@@ -14,3 +15,22 @@ class ClienteCreateView(CreateView):
 class ClienteListView(ListView):
     model = Cliente
     template_name = "lista_clientes.html"
+
+
+class ClienteUpdatView(UpdateView):
+    model = Cliente
+    fields = ("nome", "profissao", "data_aniversario")
+    template_name = "form_cliente.html"
+    success_url = reverse_lazy("lista_clientes")
+
+
+class ClienteDetailView(DetailView):
+    model = Cliente
+    template_name = "lista_cliente.html"
+    context_object_name = "cliente"
+
+
+class ClienteDeletelView(DeleteView):
+    model = Cliente
+    template_name = "remover_cliente.html"
+    success_url = reverse_lazy("lista_clientes")
