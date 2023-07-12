4ab7f1a55ddc8d48cee82b7f9cde25d4e304b489
Willian Alves Fonseca <willian.alves2109@gmail.com>
2023-07-12 16:11:21 -0300

    Organizando projeto

--- tw-django-fundamentos/parte_01/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import form_cliente, horario_atual
-
-urlpatterns = [
-    path('horario_atual', horario_atual),
-    path('form_cliente', form_cliente),
-]
