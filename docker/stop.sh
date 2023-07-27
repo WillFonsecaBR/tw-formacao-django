ea511ef51c8727910177f07dfce465104d6cbdea
Willian Alves Fonseca <willian.alves2109@gmail.com>
2023-07-27 13:18:14 -0300

    Removendo arquivos do docker mysql do projeto

--- docker/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONTAINERS_DOCKER=$(docker ps -q)
-
-echo "Parando containers..."
-docker container stop $CONTAINERS_DOCKER
-
-echo "Removendo containers..."
-docker container rm $CONTAINERS_DOCKER
